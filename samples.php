9f69b50691d7ec56217e5ddc30785c44952bf531
Caleb Stauffer <develop+github@calebstauffer.com>
2019-12-26 21:28:09 -0500

    adjust samples to use DateTime objects

--- samples.php
+++ samples.php
@@ -2,8 +2,8 @@
 
 # Register business hours Limit.
 Limit::register( 'business-hours', array(
-	date_create( '9:00 am', wp_timezone() )->format( 'U' ),
-	date_create( '5:00 pm', wp_timezone() )->format( 'U' ),
+	date_create( '9:00 am', wp_timezone() ),
+	date_create( '5:00 pm', wp_timezone() ),
 ) );
 
 # Register non-business hours Limit.
